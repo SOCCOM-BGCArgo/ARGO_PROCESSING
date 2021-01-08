a00173b7bc9a89f9e5a1bd4bcb59cbfa10296416
tmaurer <tmaurer@mbari.org>
2021-01-08 07:52:47 -0800

    Update to GLODAPv2_2020 (Olson et al., 2020)
    Update Glodapv2 to 2020v.  mat file added and parser updated.

--- MFILES/GLODAP/get_GLODAPv2_local.m
+++ MFILES/GLODAP/get_GLODAPv2_local.m
@@ -51,7 +51,8 @@ function d = get_GLODAPv2_local(datadir, track, tol, depth_bnds)
 % GLODAP_dir          = [user_dir,'\Documents\MATLAB\ARGO_PROCESSING\', ...
 %                       'DATA\GLODAP\'];
 % GLODAP_fn           = 'GLODAPv2 Merged Master 102716.mat';
-GLODAP_fn           = 'GLODAPv2.2019_Merged_Master_File.mat';
+%GLODAP_fn           = 'GLODAPv2.2019_Merged_Master_File.mat';
+GLODAP_fn			= 'GLODAPv2.2020_Merged_Master_File.mat';
 %GLODAP_expocode_fn  = 'GLODAPv2_ExpoCodes_102716.txt';
 
 % ***********************************************************************
